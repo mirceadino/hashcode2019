f8482cb24669524b7dcf4482777968d4a64ba5d1
mirko <mirceadino97@gmail.com>
2019-02-28 00:27:02 +0200

    Setup commandline flags.

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,23 @@ using namespace std;
 #define dbg(x) (cout<<#x<<" = "<<(x)<<'\n')
 
 typedef long long int lld;
-const int INF = (1LL << 30) - 1;
-const lld LINF = (1LL << 62) - 1;
+/* const int INF = (1LL << 30) - 1; */
+/* const lld LINF = (1LL << 62) - 1; */
 
-int main() {
+int main(int argc, char** argv) {
   cin.sync_with_stdio(false);
 
+  int FLAGS_age;
+  if (argc <= /* #flags = */ 1) {
+    fprintf(stderr, "usage: %s <age>\n", argv[0]);
+    return -1;
+  } else {
+    sscanf(argv[1], "%d", &FLAGS_age);
+  }
+
   Dog dog;
-  dog.age = 3;
-  cout << dog.age << endl;
+  dog.age = FLAGS_age;
+  printf("The dog is %d.\n", dog.age);
 
   return 0;
 }
-
