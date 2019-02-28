e3ec6ca964d4f07f02c4c573339c4ed8be89d8b9
mirko <mirceadino97@gmail.com>
2019-03-01 00:28:03 +0200

    cleanup in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,11 @@
 
 using namespace std;
 
-#define dbg(x) (cout<<#x<<" = "<<(x)<<'\n')
-
-typedef long long int lld;
-/* const int INF = (1LL << 30) - 1; */
-/* const lld LINF = (1LL << 62) - 1; */
-
-int main(int argc, char** argv) {
-  cin.sync_with_stdio(false);
-
-  /* int FLAGS_age; */
-  if (argc <= /* #flags = */ 0) {
-    fprintf(stderr, "usage: %s\n", argv[0]);
-    return -1;
-  } else {
-    /* sscanf(argv[1], "%d", &FLAGS_age); */
-  }
-
+int main() {
   InputReader input;
   OutputWriter output;
   input.Read();
   unique_ptr<Solver> solver = make_unique<SlideSolver>(input, output);
-  /* unique_ptr<Solver> solver = make_unique<HorizontalGreedySolver>(input, output); */
   solver->Solve();
   output.Write();
 
